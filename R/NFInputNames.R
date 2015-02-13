72e1acfb8777adb82ecbd3c623e678ebedc085a9
Alan Butler <rabutler@usbr.gov>
2015-02-12 17:12:40 -0700

    Updated documentation.
    Updated documentation.

--- R/NFInputNames.R
+++ R/NFInputNames.R
@@ -1,5 +1,25 @@
-
-CRSSNewNFInput <- function()
+#' Get vector of CRSS natural inflow file names.
+#' 
+#' \code{CRSSNFInputNames} returns a vector with the CRSS natural inflow file
+#' names in the correct node order.
+#' 
+#' This function returns a vector with the file names used by CRSS to import
+#' the natural flows. The order matches the node order used by Reclamation and
+#' CRSS and thus should not be modified. As the structure to CRSS changes, 
+#' the file names also may change. If this occurs in the future, the function
+#' will be modified to return different values for different versions of CRSS.
+#' 
+#' This is the default function used by \code{\link{createCRSSDNFInputFiles}} 
+#' for creating the input files for CRSS.
+#' 
+#' @return Vector of characters (file names).
+#' @examples
+#' fileNames <- CRSSNFInputNames()
+#' createCRSSDNFInputFiles('NaturalFlows1906-2012_withExtensions_1.8.15.xlsx','NFSinput/','2015-1-31',50,fileNames)
+#' @seealso
+#' \code{\link{createCRSSDNFInputFiles}}
+#' 
+CRSSNFInputNames <- function()
 {
 	return(c('UpperColoradoReach.Inflow',
 		'UpperColoradoAboveCameo_GainsAboveCameo.Local_Inflow',
@@ -21,7 +41,7 @@ CRSSNewNFInput <- function()
 		'SJAboveNavajo.Inflow',
 		'SanJuanTributaries.Inflow',
 		'SanJuanPowell_GainsAboveLeesFerry.Local_Inflow',
-		'CoRivPowellToLittleCORiv_PariaGains.Local_Inflow',
+		'PariaRiver.Inflow',
 		'LittleColoradoRiver.Inflow',
 		'CoRivLittleCOToVirgin_GainsAboveGC.Local_Inflow',
 		'VirginRiver.Inflow',
@@ -32,39 +52,8 @@ CRSSNewNFInput <- function()
 		'AboveImperialDamColoradoR_GainsOnColoRAboveImperialDam.Local_Inflow'))
 }
 
-CRSSOldNFInput <- function()
-{
-	return(c('UpperColoradoReach.Inflow',
-		'UpperColoradoReach_GainsAboveCameo.Local_Inflow',
-		'TaylorPark.Inflow',
-		'TaylorAboveBlueMesa_GainsAboveBlueMesa.Local_Inflow',
-		'GunnisonRiverAboveCrystal_GainsAboveCrystal.Local_Inflow',
-		'GunnisonRiverAboveGrandJunction_GainsGunnisonRiverAbvGrandJunction.Local_Inflow',
-		'DoloresRiver.Inflow',
-		'UpperColoradoReach_GainsAboveCisco.Local_Inflow',
-		'GreenRAboveFontenelle.Inflow',
-		'FontToFlamingGorge_GainsAboveGRWY.Local_Inflow',
-		'FontToFlamingGorge_GainsAboveGreendale.Local_Inflow',
-		'YampaRiver.Inflow',
-		'YampaRiver_LittleSnakeRInflow.Local_Inflow',
-		'DuchesneAboveStarv.Inflow',
-		'WhiteRiver.Inflow',
-		'GreenRWhiteToSanRafael_GainsAboveGreenRiverUT.Local_Inflow',
-		'SanRafaelRiver.Inflow',
-		'SJAboveNavajo.Inflow',
-		'SJBelowNavajo_GainsAboveBluff.Local_Inflow',
-		'SanJuanPowell_GainsAboveLeesFerry.Local_Inflow',
-		'CoRivPowellToVirgin_PariaGains.Local_Inflow',
-		'CoRivPowellToVirgin_LittleCoR.Local_Inflow',
-		'CoRivPowellToVirgin_GainsAboveGC.Local_Inflow',
-		'VirginRiver.Inflow',
-		'CoRivVirginToMead_GainsAboveHoover.Local_Inflow',
-		'CoRivMeadToMohave_GainsAboveDavis.Local_Inflow',
-		'CoRivMohaveToHavasu_BillWilliamsRiver.Local_Inflow',
-		'CoRivMohaveToHavasu_GainsAboveParker.Local_Inflow',
-		'AboveImperialDamColoradoR_GainsOnColoRAboveImperialDam.Local_Inflow'))
-}
 
+# not sure what the salt files are for
 CRSSOldNatSaltInput <- function()
 {
 	return(c('UpperColoradoReach.Inflow_Salt_Concentration',
