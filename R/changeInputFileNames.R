3d2f296655c5a5c4258854657f0c2204f11a5df1
Alan Butler <rabutler@usbr.gov>
2015-05-05 15:17:59 -0600

    Added NatFlow file name changing functions
    Includes:
    * New function to change natural flow file names
    * Documentation
    * See other commit for data that includes file names for old and new
    natural flow structure.

---
+++ R/changeInputFileNames.R
@@ -0,0 +1,34 @@
+#' Rename CRSS Input Files
+#' 
+#' Rename the CRSS natural flow and salt input files.
+#' 
+#' Because multiple versions of CRSS exist and the inflow locations have had their names changed
+#' in recent years, it is necessary to create files with different file names. It might be 
+#' easier to copy existing files instead of creating files from the source data, as 
+#' \code{\link{createCRSSDNFInputFiles}} does. \code{copyAndChangeNames} assumes the folders
+#' are structured for CRSS input, e.g., C:/CRSS/dmi/NFSinput/trace1/NFInput.Files. 
+#' 
+#' @param iFolder Path to the input folder cotaining trace folders.
+#' @param oFolder Path to the ouptut folder containig trace folders.
+#' @param nTrace Number of traces to process
+#' @param fromNames A vector of the file names found in iFolder/traceN.
+#' @param toNames A vector of the file names to create in oFolder/traceN.
+#' @return Nothing is returned from function. 
+#' @examples
+#' # load the common old and new natural flow files names included with the CRSSIO package.
+#' data(newNFNames)
+#' data(oldNFNames)
+#' copyAndChangeNames('C:/CRSS/dmi/NFSinputOrig/','C:/CRSS/dmi/NFSinputNew/',107,oldNFNames$Slot, newNFNames$Slot)
+
+copyAndChangeNFFileNames <- function(iFolder, oFolder, nTrace, fromNames, toNames)
+{
+  
+  for(i in 1:nTrace){
+    fromPath = paste(iFolder,'trace',i,'/',sep = '')
+    toPath = paste(oFolder,'trace',i,'/',sep = '')
+    dir.create(toPath)
+    for(j in 1:length(fromNames)){
+      file.copy(paste(fromPath,fromNames[j],sep = ''),paste(toPath, toNames[j],sep = ''))
+    }
+  }
+} 
