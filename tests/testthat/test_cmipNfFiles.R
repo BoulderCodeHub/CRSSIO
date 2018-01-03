fb13e8f31011d4b5d93f4ef6350c0966318166c0
Alan Butler <rabutler@usbr.gov>
2018-01-03 10:48:33 -0700

    fixing tests to pass checks

--- tests/testthat/test_cmipNfFiles.R
+++ tests/testthat/test_cmipNfFiles.R
@@ -2,18 +2,21 @@
 library(CRSSIO)
 context('Check that CMIP natural flow files are created correctly.')
 
-cmipDir <- file.path(system.file("tests", package = "CRSSIO"), "cmip5", "tmp")
+cmipDir <- "../cmip5/tmp"
+
+message("wd: ", getwd(), "\n")
+message("cmipDir: ", cmipDir)
 
 dir.create(cmipDir)
 on.exit(unlink(cmipDir, recursive = T))
 
 # unzip the example nc file
-ncPath <- system.file("tests", "cmip5", package = "CRSSIO")
+ncPath <- "../cmip5"
 unzip(file.path(ncPath, "test_cmip5_bcsd.zip"), exdir = ncPath)
 nc <- file.path(ncPath, "test_cmip5_bcsd.nc")
 on.exit(file.remove(nc), add = TRUE)
 
-fBad <- system.file("tests", "trace1", "DoloresRiver.Inflow", package = "CRSSIO")
+fBad <- "../trace1/DoloresRiver.Inflow"
 
 test_that('correct errors post',{
   expect_error(
@@ -29,7 +32,7 @@ test_that('correct errors post',{
   expect_error(
     crssi_create_cmip_nf_files(nc, file.path(cmipDir, "noExist"), 2018, 2020),
     paste0(file.path(cmipDir, "noExist"), " folder does not exist.", "\n", 
-           "Create the directory before calling create_crss_cmip_nf_files()")
+           "Create the directory before calling crssi_create_cmip_nf_files()")
   )
   
   expect_error(
