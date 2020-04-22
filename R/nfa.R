9d67082b3168f1c8c7cc18a7c7ca95d30445d139
Alan Butler <rabutler@usbr.gov>
2020-04-22 17:17:10 -0600

    working on `nfa`
    added methods for [, [<-, as_nfa.matrix, as.matrix.nfa, as.array.nfa, print, head, and tail

--- R/nfa.R
+++ R/nfa.R
@@ -13,8 +13,8 @@
 #' for multiple months and traces (sequences) for all 29 natural flow sites 
 #' ([nf_gage_names()]).
 #' 
-#' *Array:* Arrays should be an m x t x 29 array, where m is a number of months
-#' for a regular number of years (divisible by 12), and t is the number of 
+#' *Array:* Arrays should be an m x t x 29 array, where m is the number of
+#' months, and t is the number of 
 #' traces. If there are rownames, then they must be in "yyyy-mm" format, 
 #' otherwise an error will post. Rownames are not required, and if they are not
 #' provided will be set starting with the specified `start_yearmon` or assuming
@@ -46,9 +46,9 @@ nfa <- function(data = NA, dim = NA, flow_space = c("intervening", "total"),
 {
   flow_space <- match.arg(flow_space, c("intervening", "total"))
   assert_that(length(start_yearmon) == 1)
-  
+
   if (is.na(start_yearmon)) {
-    start_yearmon <- default_yearmon
+    start_yearmon <- default_yearmon()
   } else {
     start_yearmon <- zoo::as.yearmon(start_yearmon)
   }
@@ -67,7 +67,6 @@ nfa <- function(data = NA, dim = NA, flow_space = c("intervening", "total"),
     } else {
       # if dim is not NA, then it should be length 2
       assert_that(is.numeric(dim) && length(dim) == 2)
-      assert_that(dim[1] %% 12 == 0)
       
       mm <- ym_to_yyyymm(ym_seq(start_yearmon, dim[1]))
       tt <- trace_names(dim[2])
@@ -83,51 +82,46 @@ nfa <- function(data = NA, dim = NA, flow_space = c("intervening", "total"),
     
   } else {
     # dim should be NA (assumed based on data)
-    x <- as_nfa(data, flow_space)
+    x <- as_nfa(data, flow_space, start_yearmon = start_yearmon)
   }
   
   x
 }
 
 #' @export
-as_nfa <- function(x, flow_space, ...)
+as_nfa <- function(x, flow_space = c("intervening", "total"), 
+                   start_yearmon = NA, ...)
 {
   UseMethod("as_nfa")
 }
 
-# Maybe don't need this one??
 as_nfa.default <- function(x, ...)
 {
-  stop("as_nfa() is not implemented for object of class: ", class(x),
+  stop("as_nfa() is not implemented for an object of class: ", class(x),
        "\nSee ?nfa for details.")
 }
 
-as_nfa.matrix <- function(x, ...)
-{
-  
-}
-
-as_nfa.array <- function(x, flow_space, start_yearmon = NA, ...)
+as_nfa.array <- function(x, flow_space = c("intervening", "total"), 
+                         start_yearmon = NA, ...)
 {
   flow_space <- match.arg(flow_space, c("intervening", "total"))
   
   assert_that(length(dim(x)) == 3)
   assert_that(dim(x)[3] == 29)
-  assert_that(dim(x)[1] %% 12 == 0)
-  
+
   # are there rownames? -----------
   # if so...try to use them
   rr <- rownames(x)
   if (is.null(rr)) {
     if (is.na(start_yearmon)) {
       # use defaults
-      rownames(x) <- ym_to_yyyymm(ym_seq(default_yearmon, dim(x)[1]))
+      rownames(x) <- ym_to_yyyymm(ym_seq(default_yearmon(), dim(x)[1]))
     } else {
       # use sequence starting from start_yearmon
       rownames(x) <- ym_to_yyyymm(ym_seq(start_yearmon, dim(x)[1]))
     }
   } else {
-
+    
     # Be strict - if rownames exist, they have to be yyyy-mm format
     assertthat(
       all(grepl("\\d{4}-\\d{2}", rr)),
@@ -168,22 +162,45 @@ as_nfa.array <- function(x, flow_space, start_yearmon = NA, ...)
     }
   }
   
-  structure(x, "flow_space" = flow_space, class = c("nfa", "array"))
+  structure(x, "flow_space" = flow_space, class = c("nfa"))
 }
 
-as_nfa.data.frame <- function(x, ...)
+as_nfa.matrix <- function(x, flow_space = c("intervening", "total"), 
+                          start_yearmon = NA, ...)
 {
+  # will assume that the matrix only has one trace of data; format it as an 
+  # array, and then call as_nfa()
+  assert_that(ncol(x) == 29)
+  x <- array(
+    x, 
+    dim = c(nrow(x), 1, 29), 
+    dimnames = list(rownames(x), "Trace1", colnames(x))
+  )
   
+  as_nfa(x, flow_space = flow_space, start_yearmon = start_yearmon, ...)
 }
 
+# TODO
+as_nfa.data.frame <- function(x, ...)
+{ 
+  # what are required column names? trace may not be required, can assume 1 
+  # trace if not specified. should this work for long and wide format?
+  stop("not implemented")
+}
+
+# TODO
 as_nfa.list <- function(x, ...)
 {
-  
+  # what shape to work for? each list entry is a site? each list entry is a 
+  # trace? require list to be named? maybe that's the best route, then could 
+  # work with either shape
+  stop("not implemented")
 }
 
+# TODO
 as_nfa.xts <- function(x, ...)
 {
-  
+  stop("not implemented")
 }
 
 # takes a vector of yearmons and converts them to strings in yyyy-mm format
@@ -199,7 +216,10 @@ ym_seq <- function(x, length)
   x + 0:(length - 1)/12
 }
 
-default_yearmon <- zoo::as.yearmon(paste0(format(Sys.Date(), "%Y"), "-01"))
+default_yearmon <- function()
+{
+  zoo::as.yearmon(paste0(format(Sys.Date(), "%Y"), "-01"))
+}
 
 # x should be coercible to yearmon; then determine if they are increasing
 are_increasing <- function(x) 
@@ -214,4 +234,134 @@ are_increasing <- function(x)
 trace_names <- function(n)
 {
   paste0("Trace", 1:n)
+}
+
+# TODO: add site argument? That then resolve to the correct k dimension?
+#' @export
+`[.nfa` <- function(x, i, j, ...) {
+  attrs <- attributes(x)
+
+  out <- unclass(x)
+  out <- out[i, j, ..., drop = FALSE]
+
+  attr(out, "flow_space") <- attrs$flow_space
+  class(out) <- "nfa"
+
+  out
+}
+
+#' @export
+`[<-.nfa` <- function(x, i, j, ...) {
+  .Class <- "array"
+  NextMethod(.Generic)
+}
+
+#' @export
+is_nfa <- function(x) inherits(x, "nfa")
+
+ntrace <- function(x)
+{
+  assert_that(is_nfa(x))
+  dim(x)[2]
+}
+
+#' @export
+print.nfa <- function(x, ...)
+{
+  cat(
+    "CRSSIO::nfa - natural flow array\n",
+    " - flow_space:  ", attr(x, "flow_space"), "\n",
+    " - # traces:    ", ntrace(x), "\n",
+    " - time period: ", rownames(x)[1], " to ", tail(rownames(x), 1), "\n",
+    "Trace 1:\n"
+  )
+  print(head(x, 3))
+  cat("\n...\n")
+  
+  invisible(x)
+}
+
+#' @export
+head.nfa <- function(x, n = 6L, trace = 1, ...)
+{
+  assert_that(trace <= ncol(x))
+  assert_that(length(n) == 1 && is.numeric(n))
+  
+  if (n > 0) {
+    n <- min(n, nrow(x))
+    n <- 1:n
+  } else {
+    n <- min(nrow(x), abs(n))
+    if (n == nrow(x)) {
+      n <- 0
+    } else {
+      n <- 1:(nrow(x) - n)
+    }
+  }
+  
+  as.matrix(x[n, trace, ])
+}
+
+#' @export
+tail.nfa <- function(x, n = 6L, trace = 1, ...)
+{
+  assert_that(trace <= ncol(x))
+  assert_that(length(n) == 1 && is.numeric(n))
+
+  if (n > 0) {
+    n <- min(n, nrow(x))
+    n <- (nrow(x) - n + 1):nrow(x)
+  } else {
+    n <- min(nrow(x), abs(n))
+    if (n == nrow(x)) {
+      n <- 0
+    } else {
+      n <- (n + 1):nrow(x)
+    }
+  }
+  
+  as.matrix(x[n, trace, ])
+}
+
+#' @export
+as.array.nfa <- function(x, ...)
+{
+  unclass(x)
+}
+
+#' @export
+as.matrix.nfa <- function(x, ...)
+{
+  assert_that(
+    ncol(x) == 1,
+    msg = "Can only convert nfa to matrix if there is only one trace of data."
+  )
+  
+  unclass(x)[,,, drop = TRUE]
+}
+
+# TODO: everything below here ---------
+as.data.frame.nfa <- function(x, long = TRUE, ...)
+{
+  # create column names for trace and yearmon. return in long format which has
+  # site column. return in wide format which has one column for each site
+  stop("not implemented")
+}
+
+rbind.nfa <- function(..., deparse.level = 1)
+{
+  # complicated - will check that the time ranges are not overlapping or sparse
+  stop("not impelmented")
+}
+
+cbind.nfa <- function(..., deparse.level = 1)
+{
+  # will combine by trace, but need to check that time ranges are identical
+  stop("not implemented")
+}
+
+c.nfa <- function(...)
+{
+  # combine based on 4th dimension once we are storing intervening and total flow
+  stop("not implemented")
 }
