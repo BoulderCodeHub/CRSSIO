7cdd9771921d7057f6b4d832cccc7b1469b85e36
Alan Butler <rabutler@usbr.gov>
2020-04-21 23:31:46 -0600

    working on `nfa` object.
    finished the creation when data is already an array

--- R/nfa.R
+++ R/nfa.R
@@ -3,11 +3,26 @@
 # TODO: should this be an extension of xts? can you have 4-D xts? 
 # would it simplify summation to annual?
 # TODO: should this actually be a list with one entry for annual and one for monthly?
+# TODO: should there be an attribute that specifies if the dates mean something,
+# ex: dates mean something in historical data or in VIC data, but they are 
+# arbitrary in ism data. 
+
 #' Natural Flow Array (nfa)
 #' 
 #' `nfa()` creates a natural flow array from the specified `data`. Stores data
 #' for multiple months and traces (sequences) for all 29 natural flow sites 
 #' ([nf_gage_names()]).
+#' 
+#' *Array:* Arrays should be an m x t x 29 array, where m is a number of months
+#' for a regular number of years (divisible by 12), and t is the number of 
+#' traces. If there are rownames, then they must be in "yyyy-mm" format, 
+#' otherwise an error will post. Rownames are not required, and if they are not
+#' provided will be set starting with the specified `start_yearmon` or assuming
+#' a starting date of January of the current year. The colnames are not required
+#' but will be used in their provided form if they are specified. If they are 
+#' not specified, they will be named Trace1 - TraceN. The names of the 3rd 
+#' dimension should match [nf_gage_abbrv()] if provided. If they are not 
+#' provided, then assume that the depths match the order in [nf_gage_abbrv()].
 #' 
 #' @param data If `NA` or of length 1, creates an object with dimensions based
 #'   on `dim`. Otherwise, creates an object based on the provided data. Data 
@@ -21,28 +36,32 @@
 #'   
 #' @param flow_space Data are intervening or total flow.
 #' 
-#' @param start_year Start year to use for monthly data if `data` is length of 1.
+#' @param start_yearmon Start year and month of data. If `NA`, then assumes the
+#'   data begins in January of the current year. Should be a [zoo::yearmon] 
+#'   object or coercable to one. 
 #' 
 #' @export
 nfa <- function(data = NA, dim = NA, flow_space = c("intervening", "total"),
-                 start_year = NA)
+                 start_yearmon = NA)
 {
   flow_space <- match.arg(flow_space, c("intervening", "total"))
-  assert_that(
-    is.na(start_year) || (is.numeric(start_year) && length(start_year) == 1)
-  )
-  if (is.na(start_year))
-    start_year <- format(Sys.Date(), "%Y")
+  assert_that(length(start_yearmon) == 1)
   
-  if (is.na(data) || (length(data) == 1 && is.numeric(data))) {
+  if (is.na(start_yearmon)) {
+    start_yearmon <- default_yearmon
+  } else {
+    start_yearmon <- zoo::as.yearmon(start_yearmon)
+  }
+  
+  if (isTRUE(is.na(data)) || (length(data) == 1 && is.numeric(data))) {
     # initialize an empty nfa
-    if (is.na(dim)) {
+    if (isTRUE(is.na(dim))) {
       # just one time dimension, and one trace dimension
       x <- array(
         data = data, 
         dim = c(1, 1, 29), 
         dimnames = list(
-          paste0(start_year, '-01'), "Trace1", nf_gage_abbrv()
+          ym_to_yyyymm(start_yearmon), "Trace1", nf_gage_abbrv()
         )
       )
     } else {
@@ -50,9 +69,8 @@ nfa <- function(data = NA, dim = NA, flow_space = c("intervening", "total"),
       assert_that(is.numeric(dim) && length(dim) == 2)
       assert_that(dim[1] %% 12 == 0)
       
-      # TODO: add years to mm
-      mm <- sprintf("%02d", rep(1:12, dim[1] / 12))
-      tt <- paste0("Trace", 1:dim[2])
+      mm <- ym_to_yyyymm(ym_seq(start_yearmon, dim[1]))
+      tt <- trace_names(dim[2])
       
       x <- array(
         data = data, 
@@ -67,6 +85,8 @@ nfa <- function(data = NA, dim = NA, flow_space = c("intervening", "total"),
     # dim should be NA (assumed based on data)
     x <- as_nfa(data, flow_space)
   }
+  
+  x
 }
 
 #' @export
@@ -78,7 +98,8 @@ as_nfa <- function(x, flow_space, ...)
 # Maybe don't need this one??
 as_nfa.default <- function(x, ...)
 {
-  
+  stop("as_nfa() is not implemented for object of class: ", class(x),
+       "\nSee ?nfa for details.")
 }
 
 as_nfa.matrix <- function(x, ...)
@@ -86,9 +107,68 @@ as_nfa.matrix <- function(x, ...)
   
 }
 
-as_nfa.array <- function(x, ...)
+as_nfa.array <- function(x, flow_space, start_yearmon = NA, ...)
 {
+  flow_space <- match.arg(flow_space, c("intervening", "total"))
   
+  assert_that(length(dim(x)) == 3)
+  assert_that(dim(x)[3] == 29)
+  assert_that(dim(x)[1] %% 12 == 0)
+  
+  # are there rownames? -----------
+  # if so...try to use them
+  rr <- rownames(x)
+  if (is.null(rr)) {
+    if (is.na(start_yearmon)) {
+      # use defaults
+      rownames(x) <- ym_to_yyyymm(ym_seq(default_yearmon, dim(x)[1]))
+    } else {
+      # use sequence starting from start_yearmon
+      rownames(x) <- ym_to_yyyymm(ym_seq(start_yearmon, dim(x)[1]))
+    }
+  } else {
+
+    # Be strict - if rownames exist, they have to be yyyy-mm format
+    assertthat(
+      all(grepl("\\d{4}-\\d{2}", rr)),
+      msg = "The provided rownames are not all in yyyy-mm format."
+    )
+    
+    # check if they are increasing; if not, guess we should sort them
+    if (!are_increasing(rr)) {
+      # sort the array
+      rr_sort <- sort.int(rr, index.return = TRUE)$ix
+      x <- x[rr_sort, ,]
+    }
+  }
+  
+  # are there colnames? ---------
+  # if so use them
+  # colnames are less strict, use what there is if there is something,
+  # otherwise name them Trace1 - TraceN
+  if (is.null(colnames(x))) {
+    colnames(x) <- trace_names(dim(x)[2])
+  }
+  
+  # are there depth names? ---------------
+  # if so try to use them; they have to be nf_gage_names
+  # if they aren't assume that the gages are in correct order
+  dd <- dimnames(x)[[3]]
+  if (is.null(dd)) {
+    dimnames(x)[[3]] <- nf_gage_abbrv()
+  } else {
+    assert_that(all(dd %in% nf_gage_abbrv()) && all(nf_gage_abbrv()) %in% dd)
+    
+    # check to see if we need to reorder the depth dimension
+    nf_names <- match(dd, nf_gage_abbrv())
+    if (!all(nf_names == 1:29)) {
+      # they need to be sorted 
+      nf_i <- sort.int(nf_names, index.return = TRUE)$ix
+      x <- x[, , nf_i]
+    }
+  }
+  
+  structure(x, "flow_space" = flow_space, class = c("nfa", "array"))
 }
 
 as_nfa.data.frame <- function(x, ...)
@@ -104,4 +184,34 @@ as_nfa.list <- function(x, ...)
 as_nfa.xts <- function(x, ...)
 {
   
+}
+
+# takes a vector of yearmons and converts them to strings in yyyy-mm format
+ym_to_yyyymm <- function(x)
+{
+  format(x, "%Y-%m")
+}
+
+# takes a yearmon `x`, and creates a vector yearmons starting with x and a 
+# specified length
+ym_seq <- function(x, length)
+{
+  x + 0:(length - 1)/12
+}
+
+default_yearmon <- zoo::as.yearmon(paste0(format(Sys.Date(), "%Y"), "-01"))
+
+# x should be coercible to yearmon; then determine if they are increasing
+are_increasing <- function(x) 
+{
+  x <- zoo::as.yearmon(x)
+  i <- length(x)
+  
+  all(x[2:i] >= x[1:(i-1)])
+}
+
+# returns vector from Trace1 to Trace[n]
+trace_names <- function(n)
+{
+  paste0("Trace", 1:n)
 }
--- tests/testthat/test-nfa.R
+++ tests/testthat/test-nfa.R
@@ -3,10 +3,38 @@ test_that("nfa() with only 1 value for data", {
   expect_is(x <- nfa(), "nfa")
   expect_equal(dim(x), c(1, 1, 29))
   expect_equivalent(x[1,1,], rep(NA, 29))
+  expect_identical(attr(x, "flow_space"), "intervening")
+  expect_setequal(attr(x, "dimnames")[[3]], nf_gage_abbrv())
   
-  expect_is(x <- nfa(-99, c(24, 10), start_year = 2000), "nfa")
+  expect_is(
+    x <- nfa(-99, c(24, 10), start_yearmon = "2000-01", flow_space = "total"), 
+    "nfa"
+  )
   expect_equal(dim(x), c(24, 10, 29))
   expect_equivalent(x[1,1,], rep(-99, 29))
   expect_true(all(x == -99))
   expect_equal(rownames(x)[1], "2000-01")
+  expect_equal(tail(rownames(x), 1), "2001-12")
+  expect_identical(attr(x, "flow_space"), "total")
+  expect_setequal(attr(x, "dimnames")[[3]], nf_gage_abbrv())
+  
+  expect_is(x <- nfa(dim = c(36, 2), start_yearmon = "Oct 2005"), "nfa")
+  expect_equal(rownames(x)[1], "2005-10")
+  expect_equal(tail(rownames(x), 1), "2008-09")
 })
+
+# arrays ---------------------------
+a1 <- array(rep(1:24, 29), dim = c(24, 1, 29))
+yy <- as.numeric(format(Sys.Date(), "%Y"))
+mm <- expand.grid(sprintf("%02d", 1:12), yy:(yy+1))
+mm <- paste0(mm[,2], "-", mm[,1])
+test_that("nfa() works with arrays", {
+  expect_is(x <- nfa(a1), "nfa")
+  expect_identical(dim(x), dim(a1))
+  expect_identical(
+    dimnames(x), 
+    list(mm, "Trace1", nf_gage_abbrv())
+  )
+  expect_equal(attr(x, "flow_space"), "intervening")
+})
+
