5f24e34e81ca7bb34f21c9c3b8789915661b9455
Alan Butler <rabutler@usbr.gov>
2020-06-04 21:27:39 +0000

    update test for 2018 natural flow excel file

--- tests/testthat/test_works_with_CoRiverNF.R
+++ tests/testthat/test_works_with_CoRiverNF.R
@@ -17,7 +17,7 @@ test_that("created files from package and Excel match", {
   skip_on_travis()
   skip_on_appveyor()
   
-  xlIn <- "C:/alan/RPackages/CoRiverNF/data-raw/NaturalFlows1906-2017_3.18.2019.xlsx"
+  xlIn <- "C:/alan/NaturalFlow/current/NaturalFlows1906-2018_20200110.xlsx"
   if (!file.exists(xlIn))
     stop("You need to update the xlIn variable in test_data.R.")
   
