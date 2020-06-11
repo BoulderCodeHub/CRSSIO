d6adeced7910f4ac0a711d11b6c37cc1fcd6411a
Alan Butler <rabutler@usbr.gov>
2020-06-11 15:26:39 -0600

    finished `nfd_stats()` and `plot.nfd_stats()`
    includes doc and tests
    
    #95

--- R/nfd_stats.R
+++ R/nfd_stats.R
@@ -1,68 +1,209 @@
-plot.nfd_stats <- function(x, site, flow_space, time_step, base_units = NULL, 
-                     which = "stats", show = TRUE, ...)
+#' Compute Basic Statistics on NFD Objects
+#' 
+#' `nfd_stats()` computes six basic statistics for each trace in any [nfd] type 
+#' objects. The basic statistics are mean, variance, minimum, maximum, lag-1 
+#' correlation, and skew. The resulting statistics are stored in a special 
+#' data.frame - a `nfd_stats` object that inherits from `tbl_df` and 
+#' `data.frame`. The resulting statistics can be plotted by calling `plot()` on
+#' the resulting object.
+#' 
+#' @param x An object inheriting from [nfd].
+#' 
+#' @param site The site to compute the stats for. Numeric or name (character).
+#' 
+#' @param flow_space "total" or "intervening".
+#' 
+#' @param time_step "monthly" or "annual".
+#' 
+#' @param plot Boolean. If `TRUE` will show the plot. Otherwise, user can 
+#'   subsequently call `plot()` on the returned data.
+#' 
+#' @return `nfd_stats` data.frame. 
+#' 
+#' @seealso [nfd], [plot.nfd_stats()]
+#'
+#' @export
+nfd_stats <- function(x, site, flow_space, time_step, plot = FALSE) 
+{
+  assert_that(flow_space %in% c("total", "intervening"))
+  assert_that(time_step %in% c("annual", "monthly"))
+  assert_that(length(site) == 1) 
+  assert_that(
+    site %in% sites(x) || site %in% seq(sites(x)),
+    msg = "`site` does not exist in nfd object."
+  )
+  assert_that(length(plot) == 1 && is.logical(plot))
+  
+  x_xts <- nfd_get_site(x, site, flow_space, time_step)
+  if (time_step == "annual")
+    x_df <- nfd_ann_stats(x_xts, attr(x, "year"))
+  else
+    x_df <- nfd_mon_stats(x_xts, attr(x, "year"))
+  
+  class(x_df) <- c("nfd_stats", class(x_df))
+  
+  if (plot)
+    plot(x_df, show = TRUE)
+  
+  x_df
+}
+
+nfd_ann_stats <- function(x, year_type)
+{
+  # flow_space, time_step, and site are xts attributes
+  keep_atts <- xts::xtsAttributes(x)
+  colnames(x) <- seq(ncol(x))
+  x <- xts_to_long_df(x, add_year = TRUE)
+  x <- get_nfd_stats(x, "value", "trace")
+  
+  # add attributes to df
+  x$flow_space <- keep_atts$flow_space
+  x$time_step <- keep_atts$time_step
+  x$site <- keep_atts$site
+  x$year_type <- year_type
+  
+  x
+}
+
+nfd_mon_stats <- function(x, year_type)
+{
+  # flow_space, time_step, and site are xts attributes
+  keep_atts <- xts::xtsAttributes(x)
+  colnames(x) <- seq(ncol(x))
+  x <- xts_to_long_df(x, add_month = TRUE)
+  x <- get_nfd_stats(x, "value", c("month", "trace"))
+  
+  # add attributes to df
+  x$flow_space <- keep_atts$flow_space
+  x$time_step <- keep_atts$time_step
+  x$site <- keep_atts$site
+  x$year_type <- year_type
+  
+  x
+}
+
+# adapted from get_plot_stats in knnstdisagg package 
+# (https://github.com/bouldercodehub/knnstdisagg)
+get_nfd_stats <- function(x_df, var_mutate, vars_group)
+{
+  var_name_order <- c(
+    "mean" = "Mean",
+    "stats::var" = "Variance",
+    "max" = "Maximum",
+    "min" = "Minimum",
+    "stats::cor" = "Lag-1 Correlation",
+    "skew" = "Skew"
+  )
+ 
+  res <- x_df %>%
+    dplyr::group_by_at(vars_group) %>%
+    dplyr::arrange_at("ym") %>%
+    dplyr::mutate_at(var_mutate, list("tmp" = dplyr::lag)) %>%
+    # means, standard deviation, max, min, skew, lag-1 correlation
+    dplyr::summarise_at(
+      var_mutate,
+      list(
+        ~ mean(.), ~ stats::var(.), ~ max(.), ~ min(.), ~ skew(.),
+        ~ stats::cor(., get("tmp"), use = "complete.obs")
+      )
+    ) %>%
+    tidyr::gather_(
+      "variable",
+      "value",
+      tidyselect::vars_select(names(.), -tidyselect::one_of(vars_group))
+    ) %>%
+    dplyr::mutate_at(
+      "variable",
+      list(~ factor(var_name_order[.], levels = var_name_order))
+    )
+    
+  res
+}
+
+#' @author Ken Nowak, Balaji Rajagopalan
+#' @noRd
+skew <- function(x)
+{
+  x1 <- x[!is.nan(x)]
+  n <- length(x1)
+  nfact <- n / ((n - 1) * (n - 2))
+  xm <- mean(x, na.rm = TRUE)
+  x_sd <- stats::sd(x, na.rm = TRUE)
+  skew = sum((x1 - xm)^3)
+  
+  (nfact * skew) / x_sd^3
+}
+
+nfd_stats_verify <- function(x)
+{
+  exp_cols <- c("trace", "variable", "value", "flow_space", "time_step", "site",
+                "year_type")
+  assert_that(inherits(x, c("nfd_stats", "data.frame")))
+  assert_that(all(exp_cols %in% colnames(x)))
+  
+  for (col in exp_cols[4:7]) {
+    assert_that(
+      length(unique(x[[col]])) == 1, 
+      msg = paste("Columns", col, "has more than one value: ", 
+                  paste(unique(x[[col]]), collapse = ", "),
+                  "\nThis makes the data.frame not a nfd_stats object.")
+    )
+  }
+  
+  if (x$time_step[1] == "monthly")
+    assert_that("month" %in% colnames(x))
+  
+  exp_vars <- c("Mean", "Variance", "Maximum", "Minimum", "Lag-1 Correlation",
+                "Skew")
+  
+  assert_that(all(levels(x$variable) %in% exp_vars))
+  
+  invisible(x)
+}
+
+#' Plot nfd_stats Objects
+#' 
+#' `plot.nfd_stats()` plots the resulting "base" statistics after calling 
+#' [nfd_stats()]. Statistics are plotted as boxplots across traces. Historical
+#' or some other reference statistics can be shown using `points`.
+#' 
+#' @param x An object inheriting from `nfd_stats`.
+#' 
+#' @param points An object inheriting from `nfd_stats`. Optional. Stats in this
+#'   object are shown as points instead of boxplots. 
+#'   
+#' @param base_units Optional. Used as y-axis label. 
+#' 
+#' @param show Boolean. If `TRUE` and in interactive mode, will show the plot
+#'   in the plot window.
+#'   
+#' @param ... Additional options passed to [geom_point()]. `size`, `shape`, and 
+#'   `color` can be overridden. 
+#'   
+#' @return `nfdplot` object.
+#' 
+#' @seealso [nfd_stats()], [save_nfdplot()]
+#' 
+#' @export
+plot.nfd_stats <- function(x, points = NULL, base_units = NULL, show = TRUE, 
+                           ...)
 {
   # input ok -------------------------------------------
-  assert_that(flow_sapce %in% c("total", "intervening", "both"))
-  assert_that(time_step %in% c("annual", "monthly", "both"))
-  assert_that(all(which %in% c("stats", "cdf")) || all(which %in% c(1:15)))
-  assert_that(length(site) == 1 && (site %in% sites(x) || site %in% seq(sites(x))))
+  nfd_stats_verify(x)
+  if (!is.null(points)) {
+    nfd_stats_verify(points)
+    assert_that(
+      x$time_step[1] == points$time_step[1],
+      msg = "`x` and `points` must have same time_step."
+    )
+  }
+  
   assert_that(length(show) == 1 && is.logical(show))
   
-  get_ann <- get_mon <- get_int <- get_tot <- FALSE
-  
-  if (flow_space %in% c("intervening", "both"))
-    get_int <- TRUE
-  if (flow_space %in% c("total", "both"))
-    get_tot <- TRUE
-  if (time_step %in% c("monthly", "both"))
-    get_mon <- TRUE
-  if (time_step %in% c("annual", "both"))
-    get_ann <- TRUE
-  
-  if (any(which %in% c(1:12, 14)) && !get_mon)
-    stop("Cannot create monthly plots if time_step is not monthly or both.")
-  
-  if (any(which %in% c(13, 15)) && !get_ann)
-    stop("Cannot create annual plots if time_step is not annual or both.")
-  
-  # data exists -----------------------------------------
-  # check that necesary data exists for the specified plot types
-  if (get_int && get_mon)
-    assert_that(has_monthly(x) && has_intervening(x, "monthly"))
-  if (get_tot && get_mon)
-    assert_that(has_monthly(x) && has_total(x, "monthly"))
-  if (get_int && get_ann)
-    assert_that(has_annual(x) && has_intervening(x, "annual"))
-  if (get_tot && get_ann)
-    assert_that(has_annual(x) && has_total(x, "annual"))
-  
-  # plot by time_step and flow_space ---------------------
-  gg_int_mon <- gg_int_ann <- gg_tot_mon <- gg_tot_ann <- NULL
-  if (get_int && get_mon)
-    gg_int_mon <- plot_monthly(
-      nfd_get_site(x, site, "intervening", "monthly"), 
-      which, "intervening", base_units
-    )
-  
-  if (get_tot && get_mon)
-    gg_tot_mon <- plot_monthly(
-      nfd_get_site(x, site, "total", "monthly"), 
-      which, "total", base_units
-    )
-  
-  if (get_int && get_ann)
-    gg_int_ann <- plot_annual(
-      nfd_get_site(x, site, "intervening", "annual"), 
-      which, "intervening", base_units
-    )
-  
-  if (get_tot && get_ann)
-    gg_tot_ann <- plot_annual(
-      nfd_get_site(x, site, "total", "annual"), 
-      which, "total", base_units
-    )
-  
-  gg <- c(gg_int_mon, gg_int_ann, gg_tot_mon, gg_tot_ann)
+  if (x$time_step[1] == "annual")
+    gg <- nfd_stats_plot_annual(x, points, base_units, ...)
+  else 
+    gg <- nfd_stats_plot_monthly(x, points, base_units, ...)
   
   if (show && interactive())
     print(gg)
@@ -71,32 +212,118 @@ plot.nfd_stats <- function(x, site, flow_space, time_step, base_units = NULL,
 }
 
 # return a nfdplot: list(gg)
-plot_monthly <- function(x, which, flow_space, base_units)
+nfd_stats_plot_monthly <- function(x, points, base_units, ...)
 {
+  shape <- plot_ops("shape", ...)
+  color <- plot_ops("color", ...)
+  size <- plot_ops("size", ...)
+  site <- x$site[1]
+  n_trace <- length(unique(x$trace))
+  if (x$year_type[1] == "cy")
+    mm <- 1:12
+  else 
+    mm <- c(10:12, 1:9)
   
-}
-
-plot_annual <- function(x, which, flow_space, base_units)
-{
-  make_plot <- which_ann_plots(which)
+  x$month <- factor(x$month, levels = mm)
   
-  if (make_plot["stats"]) {
-    # annual stats
+  gg <- ggplot(x, aes_string(x = "month", y = "value")) +
+    stat_boxplot_custom() +
+    facet_wrap("variable", ncol = 2, scales = "free_y")
+  
+  if (!is.null(points)) {
+    points$month <- factor(points$month, levels = mm)
+    gg <- gg +
+      geom_point(
+        data = points,
+        aes_string(x = "month", y = "value"),
+        shape = shape,
+        color = color,
+        size = size
+      )
   }
   
-  if (make_plot["cdf"]) {
-    # annual cdf
-  }
+  gg <- gg +
+    labs(
+      x = NULL,
+      title = paste(site, "- Monthly Statistics"),
+      y = paste("Base units =", base_units),
+      caption = paste(n_trace, "traces.")
+    ) +
+    scale_x_discrete(labels = month.abb[mm])
+  
+  gg <- list(gg)
+  class(gg) <- "nfdplot"
+  
+  gg
 }
 
-which_ann_plots <- function(which)
+nfd_stats_plot_annual <- function(x, points, base_units, ...)
 {
-  stats <- cdf <- FALSE
-  if ("stats" %in% which ||  15 %in% which)
-    stats <- TRUE
+  shape <- plot_ops("shape", ...)
+  color <- plot_ops("color", ...)
+  size <- plot_ops("size", ...)
+  site <- x$site[1]
+  n_trace <- length(unique(x$trace))
+  year_type <- ifelse(x$year_type[1] == "cy", "Calendar Year", "Water Year")
   
-  if ("cdf" %in% which || 13 %in% which)
-    cdf <- TRUE
+  gg <- ggplot(x, aes_string(y = "value")) +
+    stat_boxplot_custom() +
+    facet_wrap("variable", ncol = 2, scales = "free_y")
   
-  c("stats" = stats, "cdf" = cdf)
+  if (!is.null(points))
+    gg <- gg +
+      geom_point(
+        data = points,
+        aes_string(x = 0, y = "value"),
+        shape = shape,
+        color = color,
+        size = size
+      )
+    
+  gg <- gg +
+    labs(
+      x = NULL,
+      title = paste(site, "-", year_type, "Statistics"),
+      y = paste("Base units =", base_units),
+      caption = paste(n_trace, "traces.")
+    ) +
+    theme(axis.ticks.x = element_blank(), axis.text.x = element_blank()) +
+    scale_x_continuous(breaks = 0) +
+    coord_cartesian( xlim = c(-1, 1))
+  
+  gg <- list(gg)
+  class(gg) <- "nfdplot"
+  
+  gg
+}
+
+# checks ... to see if something was specified, otherwise sets it to default
+plot_ops <- function(op, ...)
+{
+  args <- list(...)
+  if (exists(op, where = args)) {
+    x <- args[[op]]
+  } else {
+    defaults <- list(
+      color = "#51B2FF",
+      size = 2,
+      shape = 17
+    )
+    
+    x <- defaults[[op]]
+  }
+  
+  x
+}
+
+#' @export
+rbind.nfd_stats <- function(..., deparse.level = 1)
+{
+  warning(paste0(
+    "Combining multiple nfd_stats objects may have unforseen consequences\n",
+    "  when plotting the data. Use with caution."
+  ))
+  
+  
+  rbind.data.frame(..., deparse.level = deparse.level, stringsAsFactors = FALSE)
 }
---
+++ tests/test-nfd_stats.R
@@ -0,0 +1,47 @@
+xx <- nfd(CoRiverNF::cyAnnTot, flow_space = "total", n_sites = 29)
+xx2 <- ism(xx, n_years_keep = 50)
+
+mm <- crss_nf(CoRiverNF::monthlyInt) 
+mm <- nfd_extract(mm, "1906/") 
+mm2 <- ism(mm, n_years_keep = 50)
+
+ann1 <- nfd_stats(xx, "LeesFerry", "total", "annual")
+ann2 <- nfd_stats(xx2, "LeesFerry", "total", "annual")
+mon1 <- nfd_stats(mm, "Cameo", "intervening", "monthly")
+mon2 <- nfd_stats(mm2, "Cameo", "intervening", "monthly")
+
+test_that("nfd_stats() returns correctly", {
+  expect_is(ann1, c("nfd_stats", "data.frame"))
+  expect_is(ann2, c("nfd_stats", "data.frame"))
+  expect_is(mon1, c("nfd_stats", "data.frame"))
+  expect_is(mon2, c("nfd_stats", "data.frame"))
+  
+  expect_identical(CRSSIO:::nfd_stats_verify(ann1), ann1)
+  expect_identical(CRSSIO:::nfd_stats_verify(ann2), ann2)
+  expect_identical(CRSSIO:::nfd_stats_verify(mon1), mon1)
+  expect_identical(CRSSIO:::nfd_stats_verify(mon2), mon2)
+  
+  expect_equal(nrow(ann1), 6)
+  expect_equal(nrow(ann2), 6 * CRSSIO:::n_trace(xx2))
+  expect_equal(nrow(mon1), 6 * 12)
+  expect_equal(nrow(mon2), 6 * 12 * CRSSIO:::n_trace(mm2))
+})
+
+test_that("plot.nfd_stats() generally works", {
+  expect_is(p1 <- plot(ann1, show = FALSE), "nfdplot")
+  expect_is(p2 <- plot(ann2, show = FALSE), "nfdplot")
+  expect_is(p3 <- plot(ann2, ann1, base_units = "acre-ft", show = FALSE), 
+            "nfdplot")
+  
+  expect_is(p4 <- plot(mon1, show = FALSE), "nfdplot")
+  expect_is(p5 <- plot(mon2, show = FALSE), "nfdplot")
+  expect_is(p6 <- plot(mon2, mon1, base_units = "acre-ft", show = FALSE), 
+            "nfdplot")
+  
+  expect_is(ggplot2::ggplot_build(p1[[1]]), "ggplot_built")
+  expect_is(ggplot2::ggplot_build(p2[[1]]), "ggplot_built")
+  expect_is(ggplot2::ggplot_build(p3[[1]]), "ggplot_built")
+  expect_is(ggplot2::ggplot_build(p4[[1]]), "ggplot_built")
+  expect_is(ggplot2::ggplot_build(p5[[1]]), "ggplot_built")
+  expect_is(ggplot2::ggplot_build(p6[[1]]), "ggplot_built")
+})
