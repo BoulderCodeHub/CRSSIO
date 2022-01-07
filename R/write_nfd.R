a9452239768c181d247e99c42e41d73ed74d685d
Alan Butler <rabutler@usbr.gov>
2022-01-07 16:44:39 -0700

    add write to excel option to write_nfd()

--- R/write_nfd.R
+++ R/write_nfd.R
@@ -1,7 +1,12 @@
 #' Create Natural Flow Files for Each Site
 #' 
-#' `write_nfd()` creates csv files for each site, flow type, and time step. 
-#' In the directory provided, there will be one folder for each flow type-time 
+#' `write_nfd()` creates csv files or an Excel file for `nfd` objects. 
+#' For csv files, a file is created for each site, flow type, and time step (see
+#' details below). For Excel files, there will be one Excel file for each flow
+#' type and time step.  
+#' 
+#' For csv files, in the directory provided, there will be one folder for each 
+#' flow type-time 
 #' step combination, and it each of those folders there will a csv files for
 #' each site. The csv files will be named based on the site names in the [nfd]
 #' object. Each csv is in time by trace format
@@ -12,7 +17,7 @@
 #'         | site1.csv
 #'         | site2.csv
 #'         | ...
-#'     | annua_total/
+#'     | annual_total/
 #'         | site1.csv
 #'         | site2.csv
 #'         | ...
@@ -23,38 +28,68 @@
 #' 
 #' @param overwrite Boolean. If `TRUE` and files already exists in 
 #'   `path`/traceN, the files will be overwritten.
+#'   
+#' @param format "csv" or "excel".
+#' 
+#' @param trace_names Overwrite the default trace names. If not `NULL`, then 
+#'   must be the same length as the number of traces. 
 #' 
 #' @export
-write_nfd <- function(x, path, overwrite = FALSE) {
+write_nfd <- function(x, path, overwrite = FALSE, format = "csv", 
+                      trace_names = NULL) {
   assert_that(is_crss_nf(x) || is_nfd(x))
   assert_that(dir.exists(path))
+  assert_that(format %in% c("csv", "excel") & length(format) == 1)
+  if (!is.null(trace_names)) {
+    assert_that(
+      length(trace_names) == n_trace(x),
+      msg = "length of `trace_names` must match the number of traces in nfd object."
+    )
+  }
+  
+  if (format == "csv") {
+    write_func <- flow_ts_to_csv
+  } else {
+    write_func <- flow_ts_to_excel
+  }
+  
   
   if (has_annual(x)) {
     if (has_total(x, "annual")) {
-      flow_ts_to_csv(x, "total", "annual", path = path, overwrite = overwrite)
+      write_func(x, "total", "annual", path = path, overwrite = overwrite,
+                 trace_names = trace_names)
     }
     
     if (has_intervening(x, "annual")) {
-      flow_ts_to_csv(x, "intervening", "annual", path = path, 
-                     overwrite = overwrite)
+      write_func(x, "intervening", "annual", path = path, 
+                     overwrite = overwrite, trace_names = trace_names)
     }
   }
   
   if (has_monthly(x)) {
     if (has_total(x, "monthly")) {
-      flow_ts_to_csv(x, "total", "monthly", path = path, overwrite = overwrite)
+      write_func(x, "total", "monthly", path = path, overwrite = overwrite,
+                 trace_names = trace_names)
     }
     
     if (has_intervening(x, "monthly")) {
-      flow_ts_to_csv(x, "intervening", "monthly", path = path, 
-                     overwrite = overwrite)
+      write_func(x, "intervening", "monthly", path = path, 
+                     overwrite = overwrite, trace_names = trace_names)
     }
   }
   
   invisible(x)
 }
 
-flow_ts_to_csv <- function(x, flow_space, time_step, path, overwrite) {
+# TODO: update this
+flow_ts_to_excel <- function(x, flow_space, time_step, path, overwrite, 
+                           trace_names) {
+  
+}
+
+# TODO: add logic for trace_names to this function
+flow_ts_to_csv <- function(x, flow_space, time_step, path, overwrite, 
+                           trace_names) {
   
   path <- file.path(path, paste0(time_step, "_", flow_space))
   if (!dir.exists(path)) {
