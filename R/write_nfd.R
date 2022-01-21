523dddcabcd31c926ca290c03766ca32750c2c2c
Alan Butler <rabutler@usbr.gov>
2022-01-21 13:15:49 -0700

    write_nfd() now can write csv or excel files
    - added tests

--- R/write_nfd.R
+++ R/write_nfd.R
@@ -21,6 +21,10 @@
 #'         | site1.csv
 #'         | site2.csv
 #'         | ...
+#'         
+#' For Excel files, there will be one file for each flow space, timestep 
+#' combination. Each file will have the sites as sheets, and each sheet will 
+#' be in time x trace format. 
 #' 
 #' @param x [nfd] or [crss_nf] object.
 #' 
@@ -33,18 +37,26 @@
 #' 
 #' @param trace_names Overwrite the default trace names. If not `NULL`, then 
 #'   must be the same length as the number of traces. 
+#'   
+#' @param insert_name String that is inserted in file name. 
 #' 
 #' @export
 write_nfd <- function(x, path, overwrite = FALSE, format = "csv", 
-                      trace_names = NULL) {
+                      trace_names = NULL, insert_name = "") {
   assert_that(is_crss_nf(x) || is_nfd(x))
   assert_that(dir.exists(path))
   assert_that(format %in% c("csv", "excel") & length(format) == 1)
+  assert_that(length(insert_name) == 1)
+  
   if (!is.null(trace_names)) {
     assert_that(
       length(trace_names) == n_trace(x),
       msg = "length of `trace_names` must match the number of traces in nfd object."
     )
+  }
+  
+  if (insert_name != '') {
+    insert_name <- paste0(insert_name, "_")
   }
   
   if (format == "csv") {
@@ -53,43 +65,71 @@ write_nfd <- function(x, path, overwrite = FALSE, format = "csv",
     write_func <- flow_ts_to_excel
   }
   
-  
   if (has_annual(x)) {
     if (has_total(x, "annual")) {
       write_func(x, "total", "annual", path = path, overwrite = overwrite,
-                 trace_names = trace_names)
+                 trace_names = trace_names, insert_name = insert_name)
     }
     
     if (has_intervening(x, "annual")) {
-      write_func(x, "intervening", "annual", path = path, 
-                     overwrite = overwrite, trace_names = trace_names)
+      write_func(x, "intervening", "annual", path = path, overwrite = overwrite, 
+                 trace_names = trace_names, insert_name = insert_name)
     }
   }
   
   if (has_monthly(x)) {
     if (has_total(x, "monthly")) {
       write_func(x, "total", "monthly", path = path, overwrite = overwrite,
-                 trace_names = trace_names)
+                 trace_names = trace_names, insert_name = insert_name)
     }
     
     if (has_intervening(x, "monthly")) {
       write_func(x, "intervening", "monthly", path = path, 
-                     overwrite = overwrite, trace_names = trace_names)
+                 overwrite = overwrite, trace_names = trace_names, 
+                 insert_name = insert_name)
     }
   }
   
   invisible(x)
 }
 
-# TODO: update this
 flow_ts_to_excel <- function(x, flow_space, time_step, path, overwrite, 
-                           trace_names) {
+                           trace_names, insert_name) {
+  if (!dir.exists(path)) {
+    dir.create(path)
+  }
   
+  ofile <- file.path(
+    path, 
+    paste0(insert_name, time_step, "_", flow_space, ".xlsx")
+  )
+  
+  if (!overwrite && file.exists(ofile)) {
+    stop(ofile, "\nexists and `overwrite` is `FALSE`.")
+  }
+  
+  # convert x (nfd) from it's format to a list, where each entry in the list
+  # is a site, and each entry is time x traces
+  # TODO: convert this to as.list.nfd at some point
+  if (is.null(trace_names)) {
+    trace_names <- paste0("Trace", seq_len(n_trace(x)))
+  }
+  
+  ss <- sites(x)
+  
+  x_lst <- lapply(ss, function(s) {
+    tmp <- nfd_get_site(x, s, flow_space, time_step)
+    colnames(tmp) <- trace_names
+    tmp <- as.data.frame(tmp)
+    tmp <- tibble::rownames_to_column(tmp, var = "timestep")
+  })
+  names(x_lst) <- ss
+  
+  writexl::write_xlsx(x_lst, path = ofile)
 }
 
-# TODO: add logic for trace_names to this function
 flow_ts_to_csv <- function(x, flow_space, time_step, path, overwrite, 
-                           trace_names) {
+                           trace_names, insert_name) {
   
   path <- file.path(path, paste0(time_step, "_", flow_space))
   if (!dir.exists(path)) {
@@ -103,7 +143,7 @@ flow_ts_to_csv <- function(x, flow_space, time_step, path, overwrite,
   
   if (!overwrite) {
     # check that files do not exist
-    all_files <- file.path(path, paste0(ss, ".csv"))
+    all_files <- file.path(path, paste0(insert_name, ss, ".csv"))
     tmp <- file.exists(all_files)
     assert_that(
       !any(tmp), 
@@ -114,13 +154,18 @@ flow_ts_to_csv <- function(x, flow_space, time_step, path, overwrite,
     )
   }
   
-  cc <- paste0("Trace", seq_len(n_trace(x)))
+  # if trace_names are provided, use them. If not, then will call them 
+  # Trace1 - TraceN
+  # already checked that trace_names is correct length if they were provided
+  if (is.null(trace_names)) {
+    trace_names <- paste0("Trace", seq_len(n_trace(x)))
+  }
   
   # write all csv files:
   tmp <- lapply(seq_along(ss), function(i) {
     tmp_data <- nfd_get_site(x, i, flow_space, time_step)
-    colnames(tmp_data) <- cc
-    f_name <- file.path(path, paste0(ss[i], ".csv"))
+    colnames(tmp_data) <- trace_names
+    f_name <- file.path(path, paste0(insert_name, ss[i], ".csv"))
     
     utils::write.csv(tmp_data, f_name, row.names = zoo::index(tmp_data))
   })
---
+++ tests/testthat/test-write_nfd.R
@@ -0,0 +1,85 @@
+
+d1 <- ism(nfd(CoRiverNF::cyAnnTot["2000/2005"]))
+d2 <- ism(nfd(CoRiverNF::monthlyInt["2000/2002"], time_step = "monthly",
+              flow_space = "intervening"))
+d2 <- nf_to_annual(d2)
+d2 <- nf_to_total(d2)
+
+p1 <- file.path(tempdir(), "d1")
+p2 <- file.path(tempdir(), "d2")
+dir.create(p1)
+dir.create(p2)
+teardown({
+  unlink(p1)
+  unlink(p2)
+})
+
+# write csv files ----------------------------
+test_that("write_nfd() works for csv files", {
+  write_nfd(d1, p1)
+  expect_true(dir.exists(file.path(p1, "annual_total")))
+  expect_true(
+    all(file.exists(
+      file.path(p1, "annual_total", paste0(nf_gage_abbrv(), ".csv"))
+    ))
+  )
+  expect_error(write_nfd(d1, p1))
+  expect_length(list.dirs(p1), 2)
+  expect_type(write_nfd(d2, p2, overwrite = TRUE), "list")
+  
+  expect_type(
+    write_nfd(d2, p2, trace_names = month.name[1:3], insert_name = "zz"),
+    "list"
+  )
+  expect_length(list.dirs(p2), 5)
+  file_names <- paste0("zz_", nf_gage_abbrv(), ".csv")
+  expect_true(
+    all(file.exists(
+      file.path(p2, "annual_total", file_names)
+    ))
+  )
+  expect_true(
+    all(file.exists(
+      file.path(p2, "annual_intervening", file_names)
+    ))
+  )
+  expect_true(
+    all(file.exists(
+      file.path(p2, "monthly_total", file_names)
+    ))
+  )
+  expect_true(
+    all(file.exists(
+      file.path(p2, "monthly_intervening", file_names)
+    ))
+  )
+  expect_error(write_nfd(d2, p2, insert_name = "zz"))
+  
+  # check trace names
+  f1 <- read.csv(file.path(p1, "annual_total", "Cameo.csv"))
+  f2 <- read.csv(file.path(p2, "annual_total", "zz_Cameo.csv"))
+  
+  expect_true(all(colnames(f1) == c("X", paste0("Trace", 1:6))))
+  expect_true(all(colnames(f2) == c("X", month.name[1:3])))
+})
+
+# write excel files --------------------------
+test_that("write_nfd() works for Excel files", {
+  expect_type(write_nfd(d1, p1, format = "excel"), "list")
+  expect_true(file.exists(file.path(p1, "annual_total.xlsx")))
+  df <- readxl::read_excel(file.path(p1, "annual_total.xlsx"), "Cameo")
+  expect_true(all(colnames(df) == c("timestep", paste0("Trace", 1:6))))
+  expect_error(write_nfd(d1, p1, format = "excel"))
+  expect_type(
+    write_nfd(
+      d2, p2, format = "excel", insert_name = "xx", 
+      trace_names = month.name[1:3]
+    ), 
+    "list"
+  )
+  file_names <- c("xx_annual_total.xlsx", "xx_annual_intervening.xlsx",
+                  "xx_monthly_total.xlsx", "xx_monthly_intervening.xlsx")
+  expect_true(all(file.exists(file.path(p2, file_names))))
+  df <- readxl::read_excel(file.path(p2, "xx_monthly_total.xlsx"), "Bluff")
+  expect_true(all(colnames(df) == c("timestep", month.name[1:3])))
+})
